5965b4ab521330bc3148ab8d4db59745215a9913
royhairul <royhairulanam19@gmail.com>
2023-12-15 09:04:52 +0700

    Mengubah alur validasi untuk Registrasi User

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\MessageBag;
 use App\Models\PJMK;
 use App\Models\User;
 
@@ -12,67 +13,121 @@ class RegisterController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function showPersonalForm()
     {
-        return view("register");
+        // Menampilkan Formulir untuk Personal Data
+        return view("register", ["page" => "register-personal"]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function showAccountForm()
     {
-        //
+        // Menampilkan Formulir untuk Account Data
+        return view("register", ["page" => "register-account"]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function showConfirmation($isCreated)
     {
-        $pjmk = $request->validate([
-            'pjmk_nim' => 'required',
-            'pjmk_nama' => 'required',
-            'pjmk_kelas' => 'required',
-            'pjmk_prodi' => 'required',
-            'pjmk_phone' => 'required',
-            'pjmk_email' => 'required',
+        return view('register', [
+            "page" => "register-confirm",
+            "isCreated" => $isCreated
         ]);
-
-        PJMK::create($pjmk);
-
-        return view("welcome");
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function validatePersonal(Request $request)
     {
-        //
+        // Custom Untuk Pesan Error
+        $customMessages = [
+            'required' => ':attribute field is required.',
+            'email' => ':attribute must be valid email address.',
+            'regex' => ':attribute field must start with "+62" or "0".',
+        ];
+
+        // Validasi Data PJMK
+        $validateDataPJMK = $request->validate([
+            'NIM' => 'required',
+            'nama' => 'required',
+            'kelas' => 'required',
+            'prodi' => 'required',
+            'phone' => ['required', 'string', 'regex:/^(\+62|0)\d{9,15}$/'],
+            'email' => 'required|email',
+        ], $customMessages);
+
+        // Jika Data Berhasil di Validasi
+        if($validateDataPJMK) {
+            // Mengambil Data dari Input
+            $data = [
+                'pjmk_nim' => $request->input('NIM'),
+                'pjmk_nama' => $request->input('nama'),
+                'pjmk_kelas' => $request->input('kelas'),
+                'pjmk_prodi' => $request->input('prodi'),
+                'pjmk_phone' => $request->input('phone'),
+                'pjmk_email' => $request->input('email'),
+            ];
+
+            // Pengecekan data pada database
+            if (PJMK::where('pjmk_nim', $data['pjmk_nim'])->exists()) {
+                return $this->showConfirmation($isCreated = false);
+            }
+
+            // Menyimpan Data ke dalama Session
+            session()->put('dataPJMK', $data);
+
+            // Redirect menampilkan formulir untuk membuat Akun
+            return redirect()->route('register.account');
+        }
+
+        // Jika Validasi Gagal
+        return back()->withErrors($validateDataPJMK);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    public function validateAccount(Request $request)
     {
-        //
-    }
+        // Custom Untuk Pesan Error
+        $customMessages = [
+            'required' => ':attribute field is required.'
+        ];
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+        // Validasi Data Account
+        $validateDataAccount = $request->validate([
+            'username' => 'required',
+            'password' => 'required|confirmed'
+        ], $customMessages);
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+
+        
+        // Jika validasi Gagal
+        if(!$validateDataAccount) {
+            return back()->withErrors($validateDataAccount);
+        }
+
+        // Mengambil dataPJMK dengan session
+        $dataPJMK = session('dataPJMK');
+
+        // Mengambil data account
+        $dataAccount = [
+            'nim' => $dataPJMK['pjmk_nim'],
+            'username' => $request->input('username'),
+            'password' => $request->input('password')
+        ];
+    
+        // Menyimpan data PJMK ke dalam tabel PJMK
+        $db_PJMK = PJMK::firstOrCreate($dataPJMK);
+
+        // Melakukan pengecekan pada database
+        if ($db_PJMK->wasRecentlyCreated)
+        {
+            // Jika belum ada pada Database
+            // Menyimpan ke dalam database
+            User::create($dataAccount);
+
+            // Redirect ke halaman 
+            return $this->showConfirmation($isCreated = true);
+        }
+
+        else 
+        {
+            // Jika sudah ada pada Database
+            return $this->showConfirmation($isCreated = false);
+        }
     }
 }
