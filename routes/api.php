9e547c98cbe0b0547d003dbb24bd87f5d05042cd
royhairul <royhairulanam19@gmail.com>
2023-12-18 08:38:19 +0700

    Membuat route untuk api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\PeminjamanController;
+use App\Http\Controllers\Api\JadwalController;
+use App\Http\Controllers\Api\PjmkController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +19,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('/register', [RegisterController::class, 'validatePJMK']);
+Route::post('/login', [LoginController::class, 'authenticate']);
+
+Route::get('/peminjaman', [PeminjamanController::class, 'show']);
+Route::post('/peminjaman/create', [PeminjamanController::class, 'store']);
+
+Route::get('/jadwal', [JadwalController::class, 'show']);
+Route::post('/jadwal/create', [JadwalController::class, 'store']);
+Route::put('/jadwal/{jadwalId}', [JadwalController::class, 'update']);
+
+Route::get('/pjmk/{id}', [PjmkController::class, 'showById']);
