4ad4c70b9fc6d1020aeaff24e8c19f48cff49593
royhairul <royhairulanam19@gmail.com>
2024-01-25 22:28:38 +0700

    Membuat Model untuk tabel tb_pengembalian

--- app/Models/Pengembalian.php
+++ app/Models/Pengembalian.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Pengembalian extends Model
 {
     use HasFactory;
+    protected $table = "tb_pengembalian";
+    protected $primaryKey = 'pengembalian_id';
+    protected $fillable = [
+            'pengembalian_id',
+            'pengembalian_pinjam',
+            'pengembalian_foto_sebelum',
+            'pengembalian_foto_sesudah',
+            'pengembalian_waktu',
+    ];
 }
