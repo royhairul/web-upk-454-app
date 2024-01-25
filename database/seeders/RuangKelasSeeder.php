1972b82eb8ba77a0b3e856184765c1ae420d657d
royhairul <royhairulanam19@gmail.com>
2024-01-25 22:30:56 +0700

    Mengubah ruangkelas_status untuk Seeder

--- database/seeders/RuangKelasSeeder.php
+++ database/seeders/RuangKelasSeeder.php
@@ -19,14 +19,44 @@ class RuangKelasSeeder extends Seeder
                 'ruangkelas_code' => 'C1.4',
                 'ruangkelas_kapasitas' => '30',
                 'ruangkelas_prodi' => 'TRPL',
-                'ruangkelas_status' => 'Digunakan'
+                'ruangkelas_status' => 'Kosong'
             ],
             [
                 'ruangkelas_code' => 'C3.3',
                 'ruangkelas_kapasitas' => '30',
+                'ruangkelas_prodi' => 'AGB',
+                'ruangkelas_status' => 'Kosong'
+            ],
+            [
+                'ruangkelas_code' => 'A1.4',
+                'ruangkelas_kapasitas' => '30',
                 'ruangkelas_prodi' => 'TRPL',
-                'ruangkelas_status' => 'Digunakan'
-            ]
+                'ruangkelas_status' => 'Kosong'
+            ],
+            [
+                'ruangkelas_code' => 'A2.3',
+                'ruangkelas_kapasitas' => '30',
+                'ruangkelas_prodi' => 'TRM',
+                'ruangkelas_status' => 'Kosong'
+            ],
+            [
+                'ruangkelas_code' => 'B4.1',
+                'ruangkelas_kapasitas' => '30',
+                'ruangkelas_prodi' => 'TRPL',
+                'ruangkelas_status' => 'Kosong'
+            ],
+            [
+                'ruangkelas_code' => 'E1.5',
+                'ruangkelas_kapasitas' => '30',
+                'ruangkelas_prodi' => 'TPHT',
+                'ruangkelas_status' => 'Kosong'
+            ],
+            [
+                'ruangkelas_code' => 'A3.5',
+                'ruangkelas_kapasitas' => '30',
+                'ruangkelas_prodi' => 'MBP',
+                'ruangkelas_status' => 'Kosong'
+            ],
         ];
 
         RuangKelas::insert($list_ruangan);
