ad1956ae481095566125e8d88a49657e9093b9fa
royhairul <royhairulanam19@gmail.com>
2024-01-25 13:36:24 +0700

    Menambahkan placeholder untuk input kelas pada halaman Register PJMK

--- resources/views/register/register-personal.blade.php
+++ resources/views/register/register-personal.blade.php
@@ -57,7 +57,7 @@
 		<div class="row-span-1">
 			<label for="kelas" class="block text-sm font-medium leading-6 text-gray-900">Kelas</label>
 			<div class="mt-2">
-				<input type="text" maxlength="2" name="kelas" id="kelas" autocomplete="given-name" value="{{ old('kelas') }}"
+				<input type="text" maxlength="2" name="kelas" id="kelas" autocomplete="given-name" value="{{ old('kelas') }}" placeholder="Contoh: 1F, 3A"
 					class="block w-full rounded-md border-0 pl-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6
 					@error('kelas') ring-rose-500  @enderror">
           @error('kelas')
