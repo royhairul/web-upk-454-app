8d69da966083a6b650094236fbb254e39fd7ce35
royhairul <royhairulanam19@gmail.com>
2024-01-25 21:51:31 +0700

    Menambahkan kolom status untuk tabel RuangKelas

--- database/migrations/2023_12_03_215450_create_ruang_kelas_table.php
+++ database/migrations/2023_12_03_215450_create_ruang_kelas_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('ruangkelas_kapasitas');
             $table->string('ruangkelas_prodi');
             $table->string('ruangkelas_status');
+            $table->boolean('status')->default(true);
             $table->timestamps();
         });
     }
