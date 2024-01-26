49abf33d201314a810aa6f5bd73ab07988028e3a
royhairul <royhairulanam19@gmail.com>
2024-01-26 14:55:19 +0700

    Menambah AdminUPKSeeder dan menghapus MataKuliahSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,9 +20,9 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             ProdiSeeder::class,
-            MataKuliahSeeder::class,
             RuangKelasSeeder::class,
             RolePermissionSeeder::class,
+            AdminUPKSeeder::class,
         ]);
     }
 }
